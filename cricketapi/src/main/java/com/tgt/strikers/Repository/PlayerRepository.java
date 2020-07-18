adbade56f13e6d34c70078658f8d23526c069759
PratilipiAich <pratilipiaich@gmail.com>
2020-07-18 09:11:53 +0530

    Added radomUUID and lombok features

--- cricketapi/src/main/java/com/tgt/strikers/Repository/PlayerRepository.java
+++ cricketapi/src/main/java/com/tgt/strikers/Repository/PlayerRepository.java
@@ -1,10 +1,15 @@
 package com.tgt.strikers.Repository;
 
 
-import com.tgt.strikers.model.players;
+import com.tgt.strikers.model.Player;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 
-public interface PlayerRepository extends CassandraRepository<players, Integer> {
+import java.util.UUID;
+
+public interface PlayerRepository extends CassandraRepository<Player, UUID> {
+
+//    @AllowFiltering
+//    Players findByCategory(String p_category);
 
 
 }
--- cricketapi/src/main/java/com/tgt/strikers/Service/PlayerService.java
+++ cricketapi/src/main/java/com/tgt/strikers/Service/PlayerService.java
@@ -1,14 +1,19 @@
 package com.tgt.strikers.Service;
 
 
+import com.datastax.driver.core.utils.UUIDs;
 import com.tgt.strikers.Exception.PlayerNotFoundException;
 import com.tgt.strikers.Repository.PlayerRepository;
-import com.tgt.strikers.model.players;
+import com.tgt.strikers.model.Player;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class PlayerService {
@@ -17,39 +22,56 @@ public class PlayerService {
     private PlayerRepository playerRepo;
 
     //-------------------------------------------------Create New Player-------------------------------------------------
-    public players createPlayer(players players) {
+    public Player createPlayer(Player player) {
 
-        return playerRepo.save(players);
+        return playerRepo.save(player);
     }
 
+
     //-------------------------------------------------Get All Players---------------------------------------------------
-    public List<players> getAllPlayer() {
+    public List<Player> getAllPlayer() {
 
         return playerRepo.findAll();
     }
 
     //-----------------------------------------------Get Player By ID ---------------------------------------------------
-    public players getPlayerById(int id) {
-        Optional<players> optionalPlayer = playerRepo.findById(id);
+    public Player getPlayerById(UUID id) {
+
+        Optional<Player> optionalPlayer = playerRepo.findById(id);
         if (!optionalPlayer.isPresent())
             throw new PlayerNotFoundException("Player Record with id " + id + " is not available");
         return playerRepo.findById(id).get();
     }
 
     //-----------------------------------------------Update Player By ID ------------------------------------------------
-    public players updatePlayerById(int id, players players) {
-        Optional<players> optionalPlayer = playerRepo.findById(id);
+    public Player updatePlayerById(UUID id, Player player) {
+
+        Optional<Player> optionalPlayer = playerRepo.findById(id);
         if (!optionalPlayer.isPresent())
             throw new PlayerNotFoundException("Player Record with id " + id + " is not available");
-        players.setPlayer_id(id);
-        return playerRepo.save(players);
+        player.setPlayerId(id);
+        return playerRepo.save(player);
     }
 
     //-------------------------------------------------Delete Player By ID ----------------------------------------------
-    public void deletePlayerById(int id) {
-        Optional<players> optionalPlayer = playerRepo.findById(id);
+    public void deletePlayerById(UUID id) {
+
+        Optional<Player> optionalPlayer = playerRepo.findById(id);
         if (!optionalPlayer.isPresent())
             throw new PlayerNotFoundException("Palyer Record with id " + id + " is not available");
         playerRepo.deleteById(id);
     }
+
+    //-----------------------------------------------Get Player By Category ---------------------------------------------------
+//    public Players getPlayerByCategory(String p_category) {
+//
+//            Players optionalPlayer = playerRepo.findByCategory(p_category);
+//            if (optionalPlayer.getP_category() == null)
+//                throw new PlayerNotFoundException("Player Record with category " + p_category + " is not available");
+//
+//            return playerRepo.findByCategory(p_category);
+//
+//    }
+
+
 }
