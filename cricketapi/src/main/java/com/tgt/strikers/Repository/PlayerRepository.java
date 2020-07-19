adb0e6c34a0a61d2ef339a10a926573e4605b190
PratilipiAich <pratilipiaich@gmail.com>
2020-07-19 21:23:10 +0530

    Added Swagger and @Column annotation

--- cricketapi/src/main/java/com/tgt/strikers/Repository/PlayerRepository.java
+++ cricketapi/src/main/java/com/tgt/strikers/Repository/PlayerRepository.java
@@ -1,12 +1,12 @@
 package com.tgt.strikers.Repository;
 
 
-import com.tgt.strikers.model.Player;
+import com.tgt.strikers.model.Players;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 
 import java.util.UUID;
 
-public interface PlayerRepository extends CassandraRepository<Player, UUID> {
+public interface PlayerRepository extends CassandraRepository<Players, UUID> {
 
 //    @AllowFiltering
 //    Players findByCategory(String p_category);
--- cricketapi/src/main/java/com/tgt/strikers/model/Players.java
+++ cricketapi/src/main/java/com/tgt/strikers/model/Players.java
@@ -1,6 +1,7 @@
 package com.tgt.strikers.model;
 
 import lombok.*;
+import org.springframework.data.cassandra.core.mapping.Column;
 import org.springframework.data.cassandra.core.mapping.PrimaryKey;
 import org.springframework.data.cassandra.core.mapping.Table;
 
@@ -16,17 +17,19 @@ import java.util.UUID;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
-public class Player {
+public class Players {
 
     @PrimaryKey
     private UUID playerId;
 
     @NotNull
     @Size(min = 3, message = "First Name must have atleast 3 characters")
+    @Column("first_name")
     private String firstName;
 
     @NotNull
     @Size(min = 2, message = "Last Name must have atleast 2 characters")
+    @Column("last_name")
     private String lastName;
 
     @NotNull
@@ -40,20 +43,25 @@ public class Player {
 
     @NotNull
     @Past
+    @Column("player_dob")
     private LocalDate playerDOB;
 
     @NotNull
+    @Column("player_role")
     private String playerRole;
 
     @NotNull
+    @Column("player_bowling_style")
     private String playerBowlingStyle;
 
     @NotNull
+    @Column("player_batting_style")
     private String playerBattingStyle;
 
     @NotNull
+    @Column("player_status")
     private String playerStatus;
-
+    
     private UUID stateTeamId;
 
     private UUID nationalTeamId;
