2423d8660aa5aadd9140e0f94b22010ac44fee2c
Sanmati RM <46448758+sanmati-rm@users.noreply.github.com>
2020-07-27 18:53:41 +0530

    Added scorer and scorerFixture api

---
+++ strikers/src/main/java/com/tgt/strikers/service/ScorerFixtureService.java
@@ -0,0 +1,72 @@
+package com.tgt.strikers.service;
+
+import com.tgt.strikers.exception.ScorerFixtureNotFoundException;
+import com.tgt.strikers.repository.ScorerFixtureRepository;
+import com.tgt.strikers.model.ScorerFixture;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class ScorerFixtureService {
+
+    @Autowired
+    private ScorerFixtureRepository scorerFixtureRepo;
+
+    //-------------------------------------------------Create New ScorerFixture-------------------------------------------------
+    public ScorerFixture createScorerFixture(ScorerFixture scorerFixture) {
+
+        return scorerFixtureRepo.save(scorerFixture);
+    }
+
+
+    //-------------------------------------------------Get All ScorerFixtures---------------------------------------------------
+    public List<ScorerFixture> getAllScorerFixture() {
+
+        return scorerFixtureRepo.findAll();
+    }
+
+    //-----------------------------------------------Get ScorerFixture By ID ---------------------------------------------------
+    public ScorerFixture getScorerFixtureById(UUID id) {
+
+        Optional<ScorerFixture> optionalScorerFixture = scorerFixtureRepo.findById(id);
+        if (!optionalScorerFixture.isPresent())
+            throw new ScorerFixtureNotFoundException("ScorerFixture Record with id " + id + " is not available");
+        return scorerFixtureRepo.findById(id).get();
+    }
+
+    //-----------------------------------------------Update ScorerFixture By ID ------------------------------------------------
+    public ScorerFixture updateScorerFixtureById(UUID id, ScorerFixture scorerFixture) {
+
+        Optional<ScorerFixture> optionalScorerFixture = scorerFixtureRepo.findById(id);
+        if (!optionalScorerFixture.isPresent())
+            throw new ScorerFixtureNotFoundException("ScorerFixture Record with id " + id + " is not available");
+        scorerFixture.setScorerId(id);
+        return scorerFixtureRepo.save(scorerFixture);
+    }
+
+    //-------------------------------------------------Delete ScorerFixture By ID ----------------------------------------------
+    public void deleteScorerFixtureById(UUID id) {
+
+        Optional<ScorerFixture> optionalScorerFixture = scorerFixtureRepo.findById(id);
+        if (!optionalScorerFixture.isPresent())
+            throw new ScorerFixtureNotFoundException("ScorerFixture Record with id " + id + " is not available");
+        scorerFixtureRepo.deleteById(id);
+    }
+
+    //-----------------------------------------------Get ScorerFixture By Category ---------------------------------------------------
+   /*public ScorerFixtures getScorerFixtureByCategory(String p_category) {
+
+         ScorerFixtures optionalScorerFixture = scorerFixtureRepo.findByCategory(p_category);
+            if (optionalScorerFixture.getP_category() == null)
+                throw new ScorerFixtureNotFoundException("ScorerFixture Record with category " + p_category + " is not available");
+
+            return scorerFixtureRepo.findByCategory(p_category);
+
+   }*/
+
+
+}
